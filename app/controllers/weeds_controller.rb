0a39214c6796e2a42caf7e67ea4485cfab8fd27d
Bjorn Wanbo <bjornwanbo@gmail.com>
2015-07-08 19:32:39 +0200

    edit + update, and convert all to haml

--- app/controllers/weeds_controller.rb
+++ app/controllers/weeds_controller.rb
@@ -16,15 +16,26 @@ class WeedsController < ApplicationController
 	def create
 		@weed = Weed.new(weed_params)
 		if @weed.save
-			redirect_to root_path, notice: 'Weed added to inventory'
+			redirect_to root_path, notice: 'Strain added to inventory'
 		else
 			render 'new'
 		end
 	end
 
+	def edit
+	end
+
+	def update
+		if @weed.update(weed_params)
+			redirect_to weed_path(@weed), notice: 'Strain has been updated'
+		else
+			render 'edit'
+		end
+	end
+
 	def destroy
 		@weed.destroy
-		redirect_to root_path, notice: 'Weed removed'
+		redirect_to root_path, notice: 'Strain has been removed'
 	end
 
 	private
